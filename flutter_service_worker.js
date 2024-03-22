3f61c70525358ad3ff07daf4e0381196bae59fe0
matanlurey <matanlurey@users.noreply.github.com>
2024-03-22 23:14:19 +0000

    deploy: 5dd7887b493a4aec5787f38e6b32817a4031aa1d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "6e244c709c2481e130f851c18a6214e5",
-"/": "6e244c709c2481e130f851c18a6214e5",
+const RESOURCES = {"index.html": "915b8b3efb0ea4f1b515e7edea7aa574",
+"/": "915b8b3efb0ea4f1b515e7edea7aa574",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
