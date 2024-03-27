6b75a945629bbb6dca28736cb1c403d3226808a9
Matan Lurey <matan@lurey.org>
2024-03-26 18:00:21 -0700

    Restart on engine.dart.

--- packages/unlimited/lib/build.dart
+++ packages/unlimited/lib/build.dart
@@ -2,6 +2,38 @@
 ///
 /// This library provides classes for constructing and validating decks.
 ///
+/// ## Usage
+///
+/// ```dart
+/// import 'package:unlimited/build.dart';
+/// ```
+///
+/// ## Scope
+///
+/// This library provides static type checking and some low-level correctness
+/// checks. For example, the [LimitedDeck] class ensures that the deck has no
+/// more than 3 copies of any card:
+///
+/// ```dart
+/// import 'package:unlimited/catalog/sor.dart' as sor;
+///
+/// // Throws an error.
+/// final deck = LimitedDeck(
+///   base: sor.administratorsTower,
+///   leader: sor.directorKrennic,
+///   cards: [
+///     // Intentionally include 30 copies of the same card, which is invalid.
+///     for (var i = 0; i < 30; i++) sor.deathTrooper,
+///   ],
+/// );
+/// ```
+///
+/// However, these checks are intended to make the classes easy to reason about,
+/// not to be user facing or part of a user interface. For example, programs
+/// that take input to build a [LimitedDeck] should check constraints (such as
+/// no more than 3 copies) before creating the deck (and not rely on the library
+/// to throw an error).
+///
 /// ## Example
 ///
 /// Generating and building a (terrible) [LimitedDeck] for sealed play:
--- packages/unlimited/lib/engine.dart
+++ packages/unlimited/lib/engine.dart
@@ -6,12 +6,6 @@
 /// be used when building anything like a simulator, a game client, or a game
 /// visualizer.
 ///
-/// Most data structures in this library are _mutable_ where they would be
-/// modified during gameplay. For example, a [Base] can be damaged, so it's
-/// mutable. However a [Deck] is _immutable_ because it's a collection of cards
-/// that doesn't change during gameplay (you'll need to write your own logic for
-/// something like deck building).
-///
 /// ## Usage
 ///
 /// ```dart
@@ -21,51 +15,13 @@
 /// ## Scope
 ///
 /// This library provides static type checking and some low-level correctness
-/// checks. For example, the [PremierDeck] class ensures that the deck has at
-/// least 50 cards and no more than 3 copies of any card. However, it does not
-/// try to simulate the game rules, and as such is missing concepts such as
-/// "valid action", "current player", or "defeated unit", which are left to the
-/// user to implement:
+/// checks. For example, the [Base] class can never have _negative_ damage:
 ///
 /// ```dart
 /// void example(Base base) {
-///   // Go ahead and place 1000 damage on the base.
-///   base.damage(1000);
-///   print(base.damage()); // 1000, even though in game rules the game is over.
-///
-///   // Ok, now heal it back to 30.
-///   base.damage(-1000);
-///   print(base.damage); // 30.
-/// }
-/// ```
-///
-/// In addition, this library does not provide any user interface or invalid
-/// input handling beyond throwing errors that are not expected to be caught.
-/// For example, programs that take input to build a [PremierDeck] should check
-/// constraints (such as at least 50 cards, no more than 3 copies) before
-/// creating the deck:
-///
-/// ```dart
-/// import 'package:unlimited/core.dart';
-/// import 'package:unlimited/engine.dart';
-///
-/// void example(LeaderCard leader, BaseCard base, List<DeckCard> cards) {
-///   if (cards.length < 50) {
-///     print('Deck must have at least 50 cards.');
-///     return;
-///   }
-///   // ...
+///   print(base.damage()); // 0, starting off undamaged.
+///   print(base.damage(10)); // 10, taking 10 damage.
+///   print(base.damage(-20)); // 0, cannot have negative damage.
 /// }
 /// ```
 library engine;
-
-// Imported for documentation purposes.
-import 'package:unlimited/src/build/deck.dart';
-import 'package:unlimited/src/engine/state.dart';
-
-export 'src/build/deck.dart';
-export 'src/engine/game.dart';
-export 'src/engine/id.dart';
-export 'src/engine/player.dart';
-export 'src/engine/state.dart';
-export 'src/engine/zone.dart';
